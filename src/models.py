78cc869ff8d684f05382d8b8b40287de8dfb1d03
Moyra_sc <noemisolac@gmail.com>
2022-08-10 19:29:00 +0000

    starting to create classes

--- src/models.py
+++ src/models.py
@@ -8,23 +8,39 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(30), nullable=False)
+    last_name = Column(String(30), nullable=False)
+    last_name_2 = Column(String(30), nullable=False)
+    email = Column(String(30), nullable=False)
+    password = Column(String(12), nullable=False)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name_planets = Column(String(30))
+    users = relationship("users", backref="planets", lazy=True)
+    fk_planets = Column(Integer, ForeignKey('users.id'))
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name_characters = Column(String(30))
+    user = relationship("user", backref="characters", lazy=True)
+    fk_characters = Column(Integer, ForeignKey('users.id'))
+    
+    # person_id = Column(Integer, ForeignKey('person.id'))
+    # person = relationship(Person)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    name_planets = Column(String(30))
+    name_characters = Column(String(30))
+    name_user = Column(String(30))    
 
     def to_dict(self):
         return {}
